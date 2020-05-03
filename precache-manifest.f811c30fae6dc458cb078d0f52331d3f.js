b20ac2e90539b170e95710b6a4a24d32dda2eb16
ejazsayyad <ejazsayyad4@gmail.com>
2020-05-03 19:51:27 +0530

    Updates

---
+++ precache-manifest.f811c30fae6dc458cb078d0f52331d3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01d87ce1275e114de52687e822c0be7",
+    "url": "/XpenseAppReactVersion/index.html"
+  },
+  {
+    "revision": "3e5d79a2b277956da801",
+    "url": "/XpenseAppReactVersion/static/js/2.bfdfdd94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XpenseAppReactVersion/static/js/2.bfdfdd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc75bc236358eea6e95e",
+    "url": "/XpenseAppReactVersion/static/js/main.5182eb9b.chunk.js"
+  },
+  {
+    "revision": "14eee2511e799d4613a3",
+    "url": "/XpenseAppReactVersion/static/js/runtime-main.1c8fde55.js"
+  }
+]);
